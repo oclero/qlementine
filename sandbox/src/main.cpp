b8eb54034b6fd36aecc4adfc395885525dad0894
hunt978 <bootsing.hoo@gmail.com>
2022-09-21 17:32:29 +0800

    [fix] has to put qApplication first before everything else, or could not load icon(resource system is fine).

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -11,15 +11,15 @@
 #define CSD_WINDOW 1
 
 int main(int argc, char* argv[]) {
+    QApplication qApplication(argc, argv);
+
 	QGuiApplication::setApplicationDisplayName("sandbox");
 	QCoreApplication::setApplicationName("sandbox");
 	QGuiApplication::setDesktopFileName("sandbox");
 	QCoreApplication::setOrganizationName("oclero");
 	QCoreApplication::setOrganizationDomain("olivierclero.com");
 	QCoreApplication::setApplicationVersion("1.0.0");
-  QIcon icon(QStringLiteral(":/qlementine_icon.ico"));
-  QApplication qApplication(argc, argv);
-  QApplication::setWindowIcon(QIcon(QStringLiteral(":/qlementine_icon.ico")));
+    QApplication::setWindowIcon(QIcon(QStringLiteral(":/qlementine_icon.ico")));
 
 	// Set custom QStyle.
 #if USE_CUSTOM_STYLE
