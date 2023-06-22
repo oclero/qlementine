aa9745d1f1765b9035837f717639e0e8b4982ec1
Olivier Cléro <oclero@hotmail.com>
2023-06-22 14:25:44 +0000

    Fix calls order for QApplication attributes

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -11,16 +11,19 @@
 //#define CSD_WINDOW 0
 
 int main(int argc, char* argv[]) {
+  // Must be set before creating a QApplication.
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+
   QApplication qApplication(argc, argv);
 
+  // Must be set after creating a QApplication.
   QGuiApplication::setApplicationDisplayName("sandbox");
   QCoreApplication::setApplicationName("sandbox");
   QGuiApplication::setDesktopFileName("sandbox");
   QCoreApplication::setOrganizationName("oclero");
   QCoreApplication::setOrganizationDomain("olivierclero.com");
   QCoreApplication::setApplicationVersion("1.0.0");
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
   QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
   QApplication::setWindowIcon(QIcon(QStringLiteral(":/qlementine_icon.ico")));
 
