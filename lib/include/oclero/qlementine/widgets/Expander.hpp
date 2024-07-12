1f48b4c9b9c3a17aab2f5c2d5379254040cde169
Olivier Cléro <oclero@hotmail.com>
2024-07-12 15:28:31 +0200

    Add horizontal mode for Expander widget

--- lib/include/oclero/qlementine/widgets/Expander.hpp
+++ lib/include/oclero/qlementine/widgets/Expander.hpp
@@ -13,6 +13,7 @@ class Expander : public QWidget {
   Q_OBJECT
 
   Q_PROPERTY(bool expanded READ expanded WRITE setExpanded NOTIFY expandedChanged)
+  Q_PROPERTY(Qt::Orientation orientation READ orientation WRITE setOrientation NOTIFY orientationChanged)
 
 public:
   Expander(QWidget* parent = nullptr);
@@ -20,6 +21,10 @@ public:
   bool expanded() const;
   Q_SLOT void setExpanded(bool expanded);
   Q_SIGNAL void expandedChanged();
+
+  Qt::Orientation orientation() const;
+  Q_SLOT void setOrientation(Qt::Orientation orientation);
+  Q_SIGNAL void orientationChanged();
 
   QWidget* content() const;
   void setContent(QWidget* content);
@@ -37,6 +42,7 @@ private:
 
 private:
   bool _expanded{ false };
+  Qt::Orientation _orientation{ Qt::Orientation::Vertical };
   QVariantAnimation _animation;
   QPointer<QWidget> _content{ nullptr };
 };
