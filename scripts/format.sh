0cce942486517c20055dfafc4ce4a38203b2e609
Olivier Cléro <oclero@hotmail.com>
2024-10-12 22:45:09 +0200

    The format.sh script now also handles sandbox and showcase apps

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find $(dirname "$0")/../lib/src $(dirname "$0")/../lib/include -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+find $(dirname "$0")/../lib/src $(dirname "$0")/../lib/include  $(dirname "$0")/../sandbox/src  $(dirname "$0")/../showcase/src -iname *.hpp -o -iname *.cpp | xargs clang-format -i
