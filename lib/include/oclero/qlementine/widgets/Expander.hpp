5cb32e84c3e73b3b8e19a7bfebbcc3d6e22c8cf2
Pieter Dewachter <pieter.dewachter@outlook.be>
2025-01-17 10:30:11 +0100

    Revert "Make sure to clear focus of any widgets contained by an Expander to avoid visual artifarcts"
    This reverts commit bb7ef72b3533787528a2bf40c27dd439ce47c5ff.

--- lib/include/oclero/qlementine/widgets/Expander.hpp
+++ lib/include/oclero/qlementine/widgets/Expander.hpp
@@ -52,6 +52,5 @@ private:
   Qt::Orientation _orientation{ Qt::Orientation::Vertical };
   QVariantAnimation _animation;
   QPointer<QWidget> _content{ nullptr };
-  QHash<QWidget*, Qt::FocusPolicy> _focusPolicies;
 };
 } // namespace oclero::qlementine
