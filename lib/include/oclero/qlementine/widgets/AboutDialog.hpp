927026d7fd79b6ed048edf3e208802aba0cf6e83
Olivier Cléro <oclero@hotmail.com>
2025-04-04 17:47:01 +0200

    Add methods to AboutDialog to set info manually

--- lib/include/oclero/qlementine/widgets/AboutDialog.hpp
+++ lib/include/oclero/qlementine/widgets/AboutDialog.hpp
@@ -10,6 +10,10 @@ class QHBoxLayout;
 namespace oclero::qlementine {
 class Label;
 
+/// @brief A dialog that displays information about the application.
+/// It includes the application icon, description, website link, license information,
+/// copyright notice, and social media links.
+/// It will automatically use the application's icon and name if not set explicitly.
 class AboutDialog : public QDialog {
   Q_OBJECT
 
@@ -17,6 +21,9 @@ public:
   explicit AboutDialog(QWidget* parent = nullptr);
   virtual ~AboutDialog() = default;
 
+  void setIcon(const QIcon& icon);
+  void setApplicationName(const QString& name);
+  void setApplicationVersion(const QString& version);
   void setDescription(const QString& description);
   void setWebsiteUrl(const QString& url);
   void setLicense(const QString& license);
@@ -27,6 +34,9 @@ private:
   void setupUi();
 
 private:
+  QLabel* _iconLabel{ nullptr };
+  Label* _appNameLabel{ nullptr };
+  QLabel* _appVersionLabel{ nullptr };
   QLabel* _appDescriptionLabel{ nullptr };
   QLabel* _websiteLabel{ nullptr };
   Label* _licenseLabel{ nullptr };
