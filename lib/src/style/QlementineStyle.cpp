2f72beda1c829c8738af73db88130506181ce5f2
Olivier Cléro <oclero@hotmail.com>
2025-05-29 11:19:51 +0200

    QComboBox: handle all kinds of item views

--- lib/src/style/QlementineStyle.cpp
+++ lib/src/style/QlementineStyle.cpp
@@ -4804,7 +4804,7 @@ void QlementineStyle::polish(QWidget* w) {
   }
 
   // Try to remove the background...
-  if (auto* itemView = qobject_cast<QTreeView*>(w)) {
+  if (auto* itemView = qobject_cast<QAbstractItemView*>(w)) {
     auto* popup = itemView->parentWidget();
     auto isComboBoxPopupContainer = popup && popup->inherits("QComboBoxPrivateContainer");
     if (isComboBoxPopupContainer) {
